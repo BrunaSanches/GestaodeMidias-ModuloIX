30a0f1b710ed5e5afe739ca442294eb884bea594
Bruna Sanches <bcpsanches@gmail.com>
2023-03-14 10:43:20 -0300

    check pag 11 certo

--- src/script11.js
+++ src/script11.js
@@ -9,6 +9,8 @@ const dropzones = document.querySelectorAll('.dropzone')
 
 
 
+
+
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart)
     card.addEventListener('drag', drag)
@@ -16,24 +18,27 @@ cards.forEach(card => {
 })
 
 function dragstart() {
-    // log('CARD: Start dragging ')
+
     dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
 
     // this = card
     this.classList.add('is-dragging')
+    this.classList.add('esconde')
 }
 
 function drag() {
-    // log('CARD: Is dragging ')
+   
 }
 
 function dragend() {
-    // log('CARD: Stop drag! ')
+
     dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
 
     // this = card
     this.classList.remove('is-dragging')
 }
+
+
 
 /** arrasta cartoes */
 dropzones.forEach( dropzone => {
@@ -44,14 +49,12 @@ dropzones.forEach( dropzone => {
 })
 
 function dragenter() {
-    // log('DROPZONE: Enter in zone ')
+ 
 }
 
 function dragover() {
     // this = dropzone
     this.classList.add('over')
-
-    // get dragging card
     const cardBeingDragged = document.querySelector('.is-dragging')
 
     // this = dropzone
@@ -59,15 +62,13 @@ function dragover() {
 }
 
 function dragleave() {
-    // log('DROPZONE: Leave ')
     // this = dropzone
     this.classList.remove('over')
+  
 
 }
 
 function drop() {
-    // log('DROPZONE: dropped ')
     this.classList.remove('over')
-    this.classList.add('esconde') 
-    
+  
 }
